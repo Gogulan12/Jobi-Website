4fb6d5b3aab7864900df3229c5744cacb533e935
Gogulan12 <gogulanravi12@gmail.com>
2023-03-22 13:36:59 -0400

    Completed Nav

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -4,40 +4,94 @@ import "./Navigation.css";
 export default function Navigation() {
   return (
     <div>
-      <h3>Jobi</h3>
-      <input type="text" placeholder="Search here..." />
-      <select name="cars" id="cars">
-        <option value="talent">Talents</option>
-        <option value="option2">option2</option>
-        <option value="option3">option3</option>
-        <option value="option4">option4</option>
-      </select>
-      <ul>
-        <li>Find Job</li>
-        <li>Find Talents</li>
-        <li>Pages</li>
-      </ul>
+      <section className="top-nav">
+        <div className="top-nav-left">
+          <div className="logo">
+            <svg
+              width="28"
+              height="29"
+              viewBox="0 0 28 29"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle
+                cx="13.95"
+                cy="14.25"
+                r="13.95"
+                fill="#00D462"
+                fill-opacity="0.34"
+              />
+            </svg>
 
-      <a href="#">Login</a>
-      <button>Register</button>
+            <svg
+              className="second-circle"
+              width="29"
+              height="29"
+              viewBox="0 0 29 29"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="14.45" cy="14.25" r="13.95" fill="#00D462" />
+            </svg>
 
-      <div className="listOfOptions">
-        <ul>
-          <li>Design & Creative</li>
-          <li>It & Development</li>
-          <li>Trending</li>
-          <li>Web & Mobile Dev</li>
-          <li>Writing</li>
-          <li>Sales & Marketing</li>
-          <li>Music & Audio</li>
-          <li>Video Animation</li>
-          <li>
-            <select name="" id="">
-              <option value="More">More</option>
+            <h3>jobi</h3>
+          </div>
+          <div className="search">
+            <input type="text" placeholder="Search here..." />
+            <select>
+              <option value="talent">Talents</option>
+              <option value="option2">option2</option>
+              <option value="option3">option3</option>
+              <option value="option4">option4</option>
             </select>
-          </li>
-        </ul>
-      </div>
+          </div>
+        </div>
+        <div className="top-nav-right">
+          <ul>
+            <li>Find Job</li>
+            <li>Find Talents</li>
+            <li>Pages</li>
+          </ul>
+
+          <a href="#">Login</a>
+          <button>Register</button>
+        </div>
+      </section>
+
+      <section className="nav-under">
+        <div className="listOfOptions">
+          <ul>
+            <li>Design & Creative</li>
+            <li>It & Development</li>
+            <li>Trending</li>
+            <li>Web & Mobile Dev</li>
+            <li>Writing</li>
+            <li>Sales & Marketing</li>
+            <li>Music & Audio</li>
+            <li>Video Animation</li>
+            <li className="more">
+              {/* <select>
+                <option value="More">More</option>
+              </select> */}
+              More
+              <svg
+                width="10"
+                height="7"
+                viewBox="0 0 10 7"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M1 1L4.66667 5L8.33333 1"
+                  stroke="black"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                />
+              </svg>
+            </li>
+          </ul>
+        </div>
+      </section>
     </div>
   );
 }
