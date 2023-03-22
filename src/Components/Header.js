76190b66d50489368d8c86e9aba18babff6754ea
Gogulan12 <gogulanravi12@gmail.com>
2023-03-22 15:32:25 -0400

    added header styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,16 +10,20 @@ export default function Header() {
     <div className="Header">
       <div className="leftSide">
         <p>#1 Online Marketplace</p>
-        <h1>Find the talents for any job.</h1>
+        <h1>
+          Find the talents <span>for any job.</span>
+        </h1>
         <h4>
           Unlock your potential with quality job & earn from world leading
           brands & co.
         </h4>
         <button>Post a job</button>
-        <p>Trusted by:</p>
-        <img src={payoneer} alt="payoneer" />
-        <img src={google} alt="google" />
-        <img src={adobe} alt="adobe" />
+        <div className="trusted">
+          <p>Trusted by:</p>
+          <img src={payoneer} alt="payoneer" />
+          <img className="google" src={google} alt="google" />
+          <img src={adobe} alt="adobe" />
+        </div>
       </div>
       <div className="rightSide">
         <img src={header} alt="image" />
