3be9fbf61fe465728c813fe6f2fea20ed9118435
Gogulan12 <gogulanravi12@gmail.com>
2023-03-23 20:29:42 -0400

    Added trending styling

--- src/Components/Trending.js
+++ src/Components/Trending.js
@@ -15,33 +15,87 @@ export default function Trending() {
         <div className="trending-Title">
           <h2>Trending Services</h2>
           <div className="trending-button">
-            <button>&lt;</button>
-            <button>&gt;</button>
+            <button className="first-button">
+              <svg
+                width="8"
+                height="13"
+                viewBox="0 0 8 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M6.83984 1.69199L1.99984 6.64191L6.83984 11.0073"
+                  stroke="#DADADA"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                />
+              </svg>
+            </button>
+            <button>
+              <svg
+                width="8"
+                height="13"
+                viewBox="0 0 8 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M1.16406 11.0073L6.00406 6.0574L1.16406 1.69197"
+                  stroke="#005025"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                />
+              </svg>
+            </button>
           </div>
         </div>
         <div className="image-list">
-          <div>
-            <p>WordPress Development.</p>
+          <div className="image-box wordpress">
+            <p>
+              WordPress <span>Development.</span>
+            </p>
           </div>
-          <div>
-            <p>Audio & Video Editing.</p>
+          <div className="image-box editing">
+            <p>
+              Audio & <span>Video Editing.</span>
+            </p>
           </div>
-          <div>
-            <p>Product & Branding Design</p>
+          <div className="image-box branding">
+            <p>
+              Product & <span>Branding Design</span>
+            </p>
           </div>
-          <div>
-            <p>Admin & Customer Support</p>
+          <div className="image-box support">
+            <p>
+              Admin & <span>Customer Support</span>
+            </p>
           </div>
         </div>
       </section>
       <section className="second">
-        <div className="title">
-          <h2>Expore the Marketplace.</h2>
-          <a href="#">Explore all fields &gt;</a>
+        <div className="marketplace-title">
+          <h2>Explore the marketplace.</h2>
+          <a href="#">
+            Explore all fields{" "}
+            <svg
+              width="8"
+              height="13"
+              viewBox="0 0 8 13"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M1.16406 11.0073L6.00406 6.0574L1.16406 1.69197"
+                stroke="#00BA56"
+                stroke-width="2"
+                stroke-linecap="round"
+              />
+            </svg>
+          </a>
         </div>
-        <div>
+        <div className="explore-list">
           <ul>
-            <li>
+            <li className="explore-list-one">
               <img src={ui} alt="" />
               <h4>UI/UX Design</h4>
               <p>12k+ Jobs</p>
