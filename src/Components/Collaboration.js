c87574b7f00843cbcc30569e6ed2f431b65ea941
Gogulan12 <gogulanravi12@gmail.com>
2023-03-24 10:57:06 -0400

    collab styling first half

--- src/Components/Collaboration.js
+++ src/Components/Collaboration.js
@@ -5,73 +5,76 @@ import "./Collaboration.css";
 export default function Collaboration() {
   return (
     <div>
-      <div className="left">
-        <h2>Collaboration with leading Brands.</h2>
-        <p>
-          We collaborate with a number of top tier companies on imagining the
-          future of work, have a look.
-        </p>
-        <button>Learn More</button>
-      </div>
-      <div className="right">
-        <svg
-          width="69"
-          height="70"
-          viewBox="0 0 69 70"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="34.4778" cy="34.8822" r="34.3114" fill="black" />
-        </svg>
-
-        <svg
-          width="110"
-          height="110"
-          viewBox="0 0 110 110"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="55" cy="55" r="55" fill="black" />
-        </svg>
-        <svg
-          width="56"
-          height="56"
-          viewBox="0 0 56 56"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="28.3742" cy="27.5339" r="27.5339" fill="black" />
-        </svg>
-        <svg
-          width="89"
-          height="89"
-          viewBox="0 0 89 89"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="44.4778" cy="44.4778" r="44.4778" fill="black" />
-        </svg>
-        <svg
-          width="89"
-          height="89"
-          viewBox="0 0 89 89"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="44.4778" cy="44.4778" r="44.4778" fill="black" />
-        </svg>
-        <svg
-          width="158"
-          height="158"
-          viewBox="0 0 158 158"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="78.6001" cy="78.6001" r="78.6001" fill="black" />
-        </svg>
-        <div>
-          <h3>100+</h3>
-          <p>Leading Brands</p>
+      <div className="collab-poster">
+        <div className="collab-img-left">
+          <h2>
+            Collaboration <br /> with leading <br /> Brands.
+          </h2>
+          <p>
+            We collaborate with a number of top tier companies <br /> on
+            imagining the future of work, have a look.
+          </p>
+          <button>Learn More</button>
+        </div>
+        <div className="collab-img-right">
+          <svg
+            width="69"
+            height="70"
+            viewBox="0 0 69 70"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="34.4778" cy="34.8822" r="34.3114" fill="black" />
+          </svg>
+          <svg
+            width="110"
+            height="110"
+            viewBox="0 0 110 110"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="55" cy="55" r="55" fill="black" />
+          </svg>
+          <svg
+            width="56"
+            height="56"
+            viewBox="0 0 56 56"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="28.3742" cy="27.5339" r="27.5339" fill="black" />
+          </svg>
+          <svg
+            width="89"
+            height="89"
+            viewBox="0 0 89 89"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="44.4778" cy="44.4778" r="44.4778" fill="black" />
+          </svg>
+          <svg
+            width="89"
+            height="89"
+            viewBox="0 0 89 89"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="44.4778" cy="44.4778" r="44.4778" fill="black" />
+          </svg>
+          <svg
+            width="158"
+            height="158"
+            viewBox="0 0 158 158"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="78.6001" cy="78.6001" r="78.6001" fill="black" />
+          </svg>
+          <div>
+            <h3>100+</h3>
+            <p>Leading Brands</p>
+          </div>
         </div>
       </div>
 
